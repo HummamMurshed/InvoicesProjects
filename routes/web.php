d3a3efdc8d33bf7077974cb6781ca482621bb29b
HummamMurshed <157974149+HummamMurshed@users.noreply.github.com>
2024-03-16 02:54:46 +0300

    Migrate Database MigrationFile

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Auth::routes();
-//
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 Route::get('/{page}', AdminController::class. '@index');
